3181e7c00682b3c01c06f7c740dba8b986c1f577
Abhinash <Abhinash@Abhinash-PC>
2018-10-08 23:51:53 +0530

    Commiting

--- webAutomationTesting/src/test/java/com/project/test/LoginTest.java
+++ webAutomationTesting/src/test/java/com/project/test/LoginTest.java
@@ -1,4 +1,19 @@
 package com.project.test;
 
+import org.apache.log4j.Logger;
+import org.testng.annotations.BeforeTest;
+
+import com.project.pages.LoginPage;
+
 public class LoginTest {
+	
+	static Logger log = Logger.getLogger(LoginTest.class);
+	
+	LoginPage loginPage = new LoginPage();
+	
+	@BeforeTest
+	public void navigateToUrl(){
+		log.info("----Before navigating to URL-----");
+		loginPage.loginURL();
+	}
 }
